7e71913e52a9a53f9488ce551e0a110a0f7497a5
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-25 09:24:50 -0500

    accepted solution

--- warmup/staircase.java
+++ warmup/staircase.java
@@ -1,17 +1,31 @@
 package warmup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class staircase {
     public static void main(String[] args) {
-        int n = 6;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j < n - 1 - i)
-                    System.out.print(" ");
-                else
-                    System.out.print("#");
-            }
-            System.out.println("");
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(5, 5, 5, 5, 5));
+        long min = -10;
+        long max = 1000000001;
+        long sum = 0;
+        arr.sort(null);
+        for (int i = 0; i < arr.size() - 1; i++) {
+            sum += arr.get(i);
         }
+        min = sum;
+        System.out.println(min);
+        sum = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            sum += arr.get(i);
+        }
+        if (sum < min)
+            min = sum;
+        else if (sum == min) {
+            min = sum;
+            max = sum;
+        } else
+            max = sum;
+        System.out.println("min = " + min + " max = " + max);
     }
 }
