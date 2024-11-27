6b0e5941f9ec5e2d3ee3164c0c841cc985b935b1
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-27 08:15:25 -0500

    accepted solution

---
+++ strings/strongPassword.java
@@ -0,0 +1,39 @@
+package strings;
+
+public class strongPassword {
+    public static void main(String[] args) {
+        String s = "E!%Z@";
+        int n = 5;
+        boolean special = false;
+        boolean up = false;
+        boolean low = false;
+        boolean digit = false;
+        int count = 4;
+
+        for (int i = 0; i < n; i++) {
+            if (up == false && s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                up = true;
+                count--;
+            }
+
+            if (low == false && s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                low = true;
+                count--;
+            }
+
+            if (digit == false && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                digit = true;
+                count--;
+            }
+            if (special == false && s.charAt(i) >= '!' && s.charAt(i) <= '-'
+                    || s.charAt(i) == '@' && special == false) {
+                special = true;
+                count--;
+            }
+        }
+        if (count + n <= 6) {
+            count = count + (6 - (n + count));
+        }
+        System.out.println(count);
+    }
+}
