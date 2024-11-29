387bca8696047b9bff9e8c02ac892464175dae11
Diya Salam <sdiya.salam.github@gmail.com>
2024-11-28 21:36:05 -0500

    accepted solution

---
+++ strings/marsExploration.java
@@ -0,0 +1,25 @@
+package strings;
+
+public class marsExploration {
+    public static void main(String[] args) {
+        String s = "SOSOOSOSOSOSOSSOSOSOSOSOSOS";
+        int result = 0;
+        int count = 0;
+        int i = 0;
+        int n = s.length() / 3;
+
+        while (count != n) {
+            if (s.charAt(i) != 'S')
+                result++;
+            i++;
+            if (s.charAt(i) != 'O')
+                result++;
+            i++;
+            if (s.charAt(i) != 'S')
+                result++;
+            i++;
+            count++;
+        }
+        System.out.println(result);
+    }
+}
